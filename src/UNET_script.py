715e66013ffc7dbf62c2f17362674194a14c02bd
douglasta2024 <80347061+douglasta2024@users.noreply.github.com>
2024-12-01 10:51:46 -0500

    changes to model_path

--- src/UNET_script.py
+++ src/UNET_script.py
@@ -25,7 +25,8 @@ from torch import from_numpy
 def generate_output():
     ### GLOBAL VARIABLES
     ROOT_PATH = os.getcwd()
-    MODEL_PATH = os.path.join("models", "model.pth")
+    print(ROOT_PATH)
+    MODEL_PATH = os.path.join(ROOT_PATH, "models", "model.pth")
     DATA_PATH = os.path.join(ROOT_PATH, "data")
     DEVICE = torch.device("cpu")
     model_weights = torch.load(MODEL_PATH, weights_only=True, map_location=DEVICE)
